8175d12e29b25800f88918138230e74c0cf07054
KANGPUNGYUN <zkdvnd@naver.com>
2023-07-25 14:47:55 +0900

    BFS DFS 알고리즘

--- JavaScript/자료구조/이진탐색/배열로이진탐색구현.js
+++ JavaScript/자료구조/이진탐색/배열로이진탐색구현.js
@@ -1,26 +1,26 @@
-const arr = [1, 1, 5, 124, 400, 599, 1004, 2876, 8712];
+// const arr = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59]
+// 만약 탐색할 배열의 값이 정렬되어 있지 않는 경우, 이진 탐색을 하기 전에 sort((a,b)=>a-b)로 정렬해야 됩니다.
 
 function binarySearch(arr, target) {
-  let start = 0;
-  let end = arr.length - 1;
-  let mid;
+  let low = 0;
+  let high = arr.length - 1;
 
-  while (start <= end) {
-    mid = parseInt((start + end) / 2);
+  while (low <= high) {
+    let mid = parseInt((low + high) / 2);
 
     if (target === arr[mid]) {
-      return mid;
+      return true; // 탐색하고자 하는 값의 인덱스를 리턴하는 경우 return mid로 수정
     } else {
       if (target < arr[mid]) {
-        end = mid - 1;
+        high = mid - 1;
       } else {
-        start = mid + 1;
+        low = mid + 1;
       }
     }
   }
-  return -1;
+  return false; // 탐색하고자 하는 값이 없을 경우를 -1로 리턴하는 경우 return -1로 수정
 }
 
-console.log(binarySearch(arr, 2876)); // 7
-console.log(binarySearch(arr, 1)); // 1
-console.log(binarySearch(arr, 500)); // -1
+// binarySearch(arr, 37) // true
+// binarySearch(arr, 1) // true
+// binarySearch(arr, 2) // false
